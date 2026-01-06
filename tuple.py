472612fe24af902e60a3a0675b0e353ca1900e64
Piyush <piyush@gmail.com>
2026-01-06 18:40:42 +0530

    Edited the Tuple file in the admin Branch.

--- tuple.py
+++ tuple.py
@@ -1,10 +1,11 @@
 tup = (1, 2, 3, 5, 4, 3, 2, 1, 1, 1, 1, 1)
 print('The Original Tuple is :', tup)
 
-no_1_times = tup.count(1)
-print('Using the Count Method :', no_1_times)
+# no_1_times = tup.count(1)
+# print('Using the Count Method :', no_1_times)
 
 index_value = tup.index(1, 5)
 print('Using the Index Method Method :', index_value)
 
-print('Hello World !')
+print('Bye World !')
+
