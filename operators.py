1768b4fa9117d2cc9479fd3f6cbc2fa54fb42f2e
Piyush <piyush@gmail.com>
2026-01-06 14:41:10 +0530

    Committing the Files on which i have performed the operations on th operators .

---
+++ operators.py
@@ -0,0 +1,9 @@
+# value_1 = 10
+# value_2 = 20
+# value_4 = value_1
+# print(value_1 is value_2)
+# print(value_1 is not value_2)
+# print(value_1 is value_4)
+# print(value_1 is not value_4)
+# print(id(value_1), id(value_4))
+# print(id(value_1), id(value_2))
