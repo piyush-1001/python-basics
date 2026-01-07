3e6eb0e5757c5f38e3d225e6e649ea7e94eac91c
Piyush <piyush@gmail.com>
2026-01-07 15:44:23 +0530

    File with pip and the try except block added.

---
+++ pip_with_try_except.py
@@ -0,0 +1,42 @@
+import camelcase
+c = camelcase.CamelCase()
+txt = "hello, see You world."
+# print(txt)
+# print(c.hump(txt))
+
+# try -> except -> else -> finally
+# try:
+#     print(x)
+# except NameError:
+#     print('The Given Variable is not Defined !')
+    # print('This is the Exception Block that we execute will he Exception occurs in the ry Block')
+# else:
+#     print('This is the Else part of the Try Except Block !')
+# finally:
+#     print('Finally block of Code is Always Executed !')
+
+# try:
+#     f= open('demo.txt')
+#     try:
+#         f.write(txt)
+#     except:
+#         print('Something went wrong when writing data to the file.')
+#     finally:
+#         f.close()
+# except:
+#     print('Something went wrong when the file was opening.')
+
+# x = -1
+# if x < 0:
+#     raise Exception("Sorry, No Numbers Below Zero")
+
+# x = "hello"
+# if not type(x) is int:
+#   raise TypeError("Only integers are allowed")
+
+# name = input("Enter a Name : ")
+# print(f'Hello, {name}')
+# print('The Type of the NAme is :', type(name))
+# age = int(input("Enter a Age : "))
+# print(f'My age is , {age}')
+# print('The Type of the Age is :', type(age))
